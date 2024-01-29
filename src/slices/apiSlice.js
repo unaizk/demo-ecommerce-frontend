44f1cb17c1762607d41400acae6b076f3c1793fc
root <root@Unaislaptop>
2024-01-29 19:34:33 +0530

    changes made

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({baseUrl: "https://demo-ecommerce.unaizk.com", credentials: "include"})
+const baseQuery = fetchBaseQuery({baseUrl: 'https://demo-ecommerce.unaizk.com', credentials: 'include'})
 
 export const apiSlice = createApi({
     baseQuery,
