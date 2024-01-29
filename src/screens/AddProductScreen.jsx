18b48d12dc3558dd833f723ef289c7138f67fd3e
root <root@Unaislaptop>
2024-01-29 22:35:33 +0530

    changes made

--- src/screens/AddProductScreen.jsx
+++ src/screens/AddProductScreen.jsx
@@ -142,7 +142,7 @@ const AddProductScreen = () => {
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-black-500"
             id="inline-image"
             type="file"
-            onChange={handleFileChange(e)}
+            onChange={handleFileChange}
           />
         </div>
       </div>
