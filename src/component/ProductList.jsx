58cfff29238f659c5facfa6a3fcc713c2eaad0dc
root <root@Unaislaptop>
2024-01-29 15:51:22 +0530

    changes made

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -15,7 +15,7 @@ const ProductList = () => {
   const [listProducts] = useListProductMutation()
   const [unlistProducts] = useUnlistProductMutation()
 
-  const PROFILE_IMAGE_DIR_PATH = 'https://demo-ecommerce.unaizk.com/productImage/';
+  const PROFILE_IMAGE_DIR_PATH = 'https://demo-ecommerce.unaizk.com/public/productImage/';
 
   useEffect(() => {
     const getAllProduct = async () => {
