bf5e0ac0a81a00a79a9241d9c72e96f488f30e54
root <root@Unaislaptop>
2024-01-30 00:07:13 +0530

    changes made

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -33,7 +33,7 @@ const ProductCard = ({ _id,name, description, price, image, category }) => {
     }
     return (
       <div className="lg:w-1/3 sm:w-full mb-8 lg:mb-0 mx-auto md:mx-0 lg:mx-0" style={{marginBottom : '30px'}} >
-      <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl mx-auto" style={{width : '300px'}}>
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl mx-auto" style={{width : '300px', height: '250px'}}>
         <div className="relative mx-4 mt-4 overflow-hidden bg-white bg-clip-border rounded-xl h-60 lg:h-96" onClick={() =>{navigateToProductDetailPage()}}>
           <img src={PROFILE_IMAGE_DIR_PATH} alt="card-image" className=" w-full h-full" />
         </div>
